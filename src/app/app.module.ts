ba3db51ecc01d8e00060bf864668c2aed9985d04
Anthony <anthonymarques@outlook.com>
2021-11-26 22:12:46 +0200

    setting up project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { RoomComponent } from './components/rooms/room/room.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent,
+    RoomComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule
