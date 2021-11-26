9cd6ed01a54a5b41404b1f4bd3d4464d6c28ce00
Anthony <anthonymarques@outlook.com>
2021-11-26 22:22:06 +0200

    firebase init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './components/rooms/rooms.component';
 import { RoomComponent } from './components/rooms/room/room.component';
-import { ServiceComponent } from './service/service.component';
+import { RoomService } from './service/service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RoomsComponent,
     RoomComponent,
-    ServiceComponent
+    RoomService
   ],
   imports: [
     BrowserModule
--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,9 +1,66 @@
-import { Injectable } from '@angular/core';
+
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Response } from "@angular/http";
+import "rxjs/Rx";
+import { AuthService } from "./auth.service";
+import { exhaustMap, take, map } from "rxjs/operators";
+import { AngularFireDatabase } from "@angular/fire/database";
+import { AngularFirestore } from "@angular/fire/firestore";
+
+import { Observable, ReplaySubject } from "rxjs/Rx";
+import { firestore } from "firebase";
+import * as firebase from "firebase";
+import { DomElementSchemaRegistry } from "@angular/compiler";
+import { Game } from "../model/game.model";
+
+let root;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class ServiceService {
+export class RoomService {
+  
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() { }
-}
+  // constructor(private http: HttpClient, private realtimeDb: AngularFireDatabase) {}
+  // constructor(private http: HttpClient, private authService:AuthService, private fireDB: AngularFireDatabase,
+  //   private fireService: AppFirebaseService ) { }
+  getEvents(): Observable<any> {
+    // this.doThis();
+    return this.firestore.collection("games").valueChanges();
+  }
+
+  doThis(data) {
+    // return this.firestore.collection('events'
+    // .add())
+    // });
+  }
+
+  getGameById(id) {
+    return this.firestore.doc('games/' + id).snapshotChanges();
+  }
+
+  // UpdateRecord(recordRow) {
+  //   let record = {};
+  //   record['Name'] = recordRow.EditName;
+  //   record['Age'] = recordRow.EditAge;
+  //   record['Address'] = recordRow.EditAddress;
+  //   this.crudService.update_Student(recordRow.id, record);
+  //   recordRow.isEdit = false;
+  // }
+
+  // getEvents() {
+  //   return this.authService.user.pipe(
+  //     // take(1),
+  //     exhaustMap(user => {
+  //       return this.http.get('https://teamfutbeta-88c44.firebaseio.com/events.json'
+  //       );
+  //     }),
+  //     map(
+  //       (response: Response) => {
+  //         const event = response;
+  //         return event;
+  //       }
+  //     ));
+  //   }
