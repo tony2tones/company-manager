4242e70fead9aa131f77606341eda132659f9f4f
Anthony <anthonymarques@outlook.com>
2021-11-27 12:18:25 +0200

    basic nav setup styles will do later

--- src/app/components/office-view.module.ts
+++ src/app/components/office-view.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { OfficeViewComponent } from './rooms/office-view.component';
 
 import { OfficeComponent } from './rooms/room/office.component';
-// import { RoomService } from './service.service';
-// import { RoomService } from './service/service.component';
 
 @NgModule({
-  // imports:[OfficeComponent],
   declarations: [OfficeViewComponent, OfficeComponent],
 })
 export class OfficeModule { }
