dc89d4157af12e7ef9ff96852c0f7f5daa59fb0e
Anthony <anthonymarques@outlook.com>
2021-11-30 20:01:20 +0200

    managed to connect to mongo DB !! excellent

--- src/app/components/rooms/office-view.component.ts
+++ src/app/components/rooms/office-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Office } from 'src/app/modals/office.modal';
+import { OfficeServices } from 'src/app/service.service';
 
 @Component({
   selector: 'app-office-view',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./office-view.component.css']
 })
 export class OfficeViewComponent implements OnInit {
+  public office: Office[] = [];
+  constructor(private officeService: OfficeServices) { }
 
-  constructor() { }
-
+  getOffices() {
+    this.officeService.getOffices().subscribe((data) => {
+      console.log('this is it',data)
+    })
+  } 
   ngOnInit(): void {
+    this.getOffices();
   }
 
 }
---
+++ src/app/modals/office.modal.ts
@@ -0,0 +1,9 @@
+export class Office {
+    _id: string;
+    companyName: string;
+    phoneNumber: number;
+    email: string;
+    officeCapacity: number;
+    address: string;
+    staff: [{}];
+}
