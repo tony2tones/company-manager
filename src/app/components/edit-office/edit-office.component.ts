a055219c1d391df04c000359cd0456abe98da8ee
Anthony <anthonymarques@outlook.com>
2021-12-06 21:45:17 +0200

    updated ui added navigation working on showing edit form with populated data
    work in progress

--- src/app/components/edit-office/edit-office.component.ts
+++ src/app/components/edit-office/edit-office.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Office } from 'src/app/modals/office.modal';
 import { OfficeServices } from 'src/app/service.service';
 // import { EventEmitter } from 'stream';
 
@@ -9,16 +11,29 @@ import { OfficeServices } from 'src/app/service.service';
   styleUrls: ['./edit-office.component.css']
 })
 export class EditOfficeComponent implements OnInit {
-  public officeProfileForm: FormGroup;
+  public editOfficeProfileForm: FormGroup;
+  public officeId: string;
+  selectedOffice: Office;
 
-  constructor(private officeService: OfficeServices, private formBuilder: FormBuilder) { }
+  constructor(private officeService: OfficeServices, private formBuilder: FormBuilder, private activeRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activeRoute.params.subscribe((room) => {
+      const id = room.roomId;
+      this.officeId = id;
+      if(this.officeId){
+        console.log('this roomId' , this.officeId);
+        this.getOfficeInfo();
+      }
+    }),
+    () => {
+
+    }
     this.createFormGroup();
   }
 
   createFormGroup() {
-    this.officeProfileForm = this.formBuilder.group({
+    this.editOfficeProfileForm = this.formBuilder.group({
       companyName: ['', Validators.required],
       phoneNumber: ['', Validators.required],
       officeCapacity: [0, Validators.required],
@@ -29,9 +44,30 @@ export class EditOfficeComponent implements OnInit {
 
   }
 
+  public getOfficeInfo(): void {
+    this.officeService.getOfficeById(this.officeId).subscribe((formData) => {
+      this.editOffice(formData);
+      // this.editOfficeProfileForm = formData.value;
+      console.log('this should be one office ', this.editOfficeProfileForm);
+
+    })
+  }
+
+  public editOffice(office:Office){
+    console.log('what is here ? ',  office);
+    // this.editOfficeProfileForm.patchValue({
+    //   companyName: office.companyName,
+    //   phoneNumber: office.phoneNumber,
+    //   officeCapacity: office.officeCapacity,
+    //   address: office.address,
+    //   email: office.email,
+    // })
+    // this.editOfficeProfileForm.controls['companyName'].setValue = office.companyName;
+  }
+
   public onSubmit() {
-    if (this.officeProfileForm.valid) {
-      this.officeService.addOffice(this.officeProfileForm.value).subscribe((data) => {
+    if (this.editOfficeProfileForm.valid) {
+      this.officeService.addOffice(this.editOfficeProfileForm.value).subscribe((data) => {
 
       }),
         (err) => console.log(err);
